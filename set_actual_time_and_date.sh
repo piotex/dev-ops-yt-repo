5155902076b2c18301db81c196bca64052c0b9f4
piotex <pkubon2@gmail.com>
2023-07-02 06:46:32 +0200

    Update set_actual_time_and_date.sh

--- set_actual_time_and_date.sh
+++ set_actual_time_and_date.sh
@@ -1,2 +1,5 @@
 current_datetime=$(curl -s "http://worldtimeapi.org/api/timezone/Europe/Warsaw" | grep -oE '"datetime":"[^"]+"' | cut -d'"' -f4)
 date -s "$current_datetime"
+
+date -s $(curl -s "http://worldtimeapi.org/api/timezone/Europe/Warsaw" | grep -oE '"datetime":"[^"]+"' | cut -d'"' -f4)
+
